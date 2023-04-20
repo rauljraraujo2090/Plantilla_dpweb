a2afe193c246ecfd9a8388b46c5e581767526d6a
vituchin1995 <115170951+vituchin1995@users.noreply.github.com>
2023-04-20 08:31:58 -0500

    psg

--- comprar.php
+++ comprar.php
@@ -1,1 +1,68 @@
+
+<?php include'includes/heider.php'?>
+
 <h1>Comprar</h1>
+
+
+<br>
+<br>
+<br>
+
+<br>
+<br>
+
+<div class="container my=3">
+
+<center>
+    <h1>REALIZAR LA COMPRA</h1> <br>
+</center>
+    
+     <div class="row">
+
+       
+      
+       
+      
+       
+      
+       
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="psgss.jpg" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+            <!--- <h5 class="card-title">Primer Equipo</h5>
+             <p class="card-text">El Paris Saint-Germain Football Club, conocido popularmente por sus siglas PSG, es una entidad polideportiva francesa con sede en París.</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>--->
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         
+           
+           <div class="card-body">
+             <h5 class="card-title">PCamiseta Stadium de la 4ª equipación del PSG 2022-23 dorsal Neymar Jr 10</h5>
+             <p class="card-text">En stock - Este artículo saldrá del almacén en 3 días laborables.</p>
+
+             <h5 class="card-title">Ahora: 107,75 US$US$10775</h5>
+             <p class="card-text">Regular: 136,00 US$US$13600</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         
+       </div>
+
+
+
+
+
+
+
+       </div> 
+       </div><br>
+
+
+<?php include'includes/footer.php'?>
