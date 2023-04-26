a2867d97acf624f31e00c92fd4eed92633bb65e3
Raul Jr <rauljraraujo2090@gmail.com>
2023-04-25 23:17:01 -0500

    63

---
+++ nosotros.php
@@ -0,0 +1,25 @@
+<?php include'includes/heider.php'?>
+
+
+<section class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 class="mb-4">Sobre Nosotros</h2>
+                <p class="mb-4">En nuestra tienda de ropas para damas, de cada temporada</p>
+                <p class="mb-4">convinacion y oufits pefecto para lucir este verano</p>
+                <p class="mb-4">convinacion y oufits pefecto para lucir este verano</p>
+                <p class="mb-4">convinacion y oufits pefecto para lucir este verano</p>
+                <p class="mb-4">convinacion y oufits pefecto para lucir este verano</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="card">
+                    <img src="img/logo.png" class="card-img-top" alt="Imagen">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include'includes/footer.php'?>
